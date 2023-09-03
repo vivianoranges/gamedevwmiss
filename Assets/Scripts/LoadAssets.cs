75b7d1ed67603a145529e5340a5acd72f816db6b
vivianoranges <137729669+vivianoranges@users.noreply.github.com>
2023-09-03 23:06:20 +1000

    70 percents

--- Assets/Scripts/LoadAssets.cs
+++ Assets/Scripts/LoadAssets.cs
@@ -14,7 +14,8 @@ public class LoadAssets : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Instantiate(redObj, new Vector3(2f, 0, 0), Quaternion.identity);
+        Instantiate(blueObj, new Vector3(-2f, 0, 0), Quaternion.identity);
     }
 
     // Update is called once per frame
