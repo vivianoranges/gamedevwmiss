c129b7f9b79e67249029a00712e62f255d2b37ae
vivianoranges <137729669+vivianoranges@users.noreply.github.com>
2023-09-04 01:03:42 +1000

    continuous?

---
+++ Assets/Scripts/ConsolePrint.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConsolePrint : MonoBehaviour
+{
+
+    private int i;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        i = 3; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        i++;
+        Debug.Log(gameObject.name + ":" + i);
+    }
+}
